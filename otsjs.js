a425397115683a402c5e3a64f472e44c11cf45f3
qbooka <128253782+qbooka@users.noreply.github.com>
2023-03-21 03:38:01 +0530

    Update otsjs.js

--- otsjs.js
+++ otsjs.js
@@ -396,20 +396,12 @@ async function sendRequest(url, data, method) {
   var url="https://qbook.pythonanywhere.com?pid="+pid
   var response = await fetch(url, {
     method: 'GET',
-  //   headers: {
-  //     'authority': 'qbooka.github.io',
-  //     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-  //     'accept-language': 'en-US,en;q=0.9',
-  //     'sec-ch-ua': '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
-  //     'sec-ch-ua-mobile': '?0',
-  //     'sec-ch-ua-platform': '"Windows"',
-  //     'sec-fetch-dest': 'document',
-  //     'sec-fetch-mode': 'navigate',
-  //     'sec-fetch-site': 'none',
-  //     'sec-fetch-user': '?1',
-  //     'upgrade-insecure-requests': '1',
-  //     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
-  // },
+    headers: {
+   
+       'mode': "no-cors",
+     
+  
+  },
 });
     // body: data,
 
